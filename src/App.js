d1c9452ee58af868b803d1800afe8327f2d82538
chandan-30 <saichandhan.yata44@gmail.com>
2022-11-15 23:41:44 +0530

    deploy form builder changes  to github

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import DndC from "./DndC";
 import  {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import './App.css';
+import { TouchBackend } from 'react-dnd-touch-backend'
+import {isMobile} from 'react-device-detect';
 //Return Initialized formBuilder set it to HTML
 function App() {
 
 
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
       <div className="row">
       <h1> FORM BUILDER</h1>
         <Components></Components>
